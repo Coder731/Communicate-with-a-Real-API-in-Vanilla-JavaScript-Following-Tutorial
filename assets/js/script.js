6fcd6730981e379d5edadf6808529404d98c7c27
Coder731 <stephenobrien1@gmail.com>
2021-08-04 14:23:45 +0000

    Add comment explaining posts key

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,5 +12,18 @@ function JavaScriptGenericFunction() {
             // which is the json formatted version
             data.posts;// begin parsing through data returned (where the data is an object called posts)
             // to allow rendering on a screen etc.
+            // This is how to parse json data in JavaScript
+            // because the post object was logged to the console.
+            // So, now, calling the key
+            // called posts
+            // Alternatively if this returned a set of guides
+            // would call .guides
+            // So it is dependent on what the API returns.
+            // This is part of why taking this in a 
+            // step by step process
+            // Because if try to go right from beginning 
+            // all the way to calling all of those 
+            // post titles for example
+            // then may not realise the way the data flow changes
         })
 }
