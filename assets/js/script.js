b798fcb55f23777eb2d6ec3e007664471ea979fc
Coder731 <stephenobrien1@gmail.com>
2021-08-04 13:56:14 +0000

    Remove After fetch and program logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,6 @@
 function JavaScriptGenericFunction() {
     console.log('Starting fetch call');
     const postsPromise = fetch('https://api.dailysmarty.com/posts')
-    console.log('After fetch call');
-    console.log(postsPromise);
-    console.log('After program has run');
 
     postsPromise// promise here on left...
         .then(data => data.json())//....returns a response on left of arrow|now convert that response to json on right of arrow
