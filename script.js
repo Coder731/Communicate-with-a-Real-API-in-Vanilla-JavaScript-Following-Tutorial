e5f7c7e809c7695475904ea58fe6d757ce3e6818
Coder731 <stephenobrien1@gmail.com>
2021-08-04 09:38:56 +0000

    Add code to 3 min 57 sec in video in repo description

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log('Starting fetch call');
+const postsPromise = fetch('https://api.dailysmarty.com/posts')
+console.log('After fetch call');
+console.log(postsPromise);
+console.log('After program has run');
