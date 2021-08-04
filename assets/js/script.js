192e6076c15c8f2baed9135d5e4b76d35657f563
Coder731 <stephenobrien1@gmail.com>
2021-08-04 13:48:49 +0000

    Add data log line and critical comment

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,12 @@ function JavaScriptGenericFunction() {
 
     postsPromise// promise here on left...
         .then(data => data.json())//....returns a response on left of arrow|now convert that response to json on right of arrow
-        .then(data)//....and then this in turn gets passed to the this then
-        //so the data in the first of the two thens on left of arrow
-        // is not the same as 
-        // data in the second then
-        // as this data in the second then is going to be referring to what got returned
-        // which is the json formatted version
+        .then(data => {//....and then this in turn gets passed to the this then
+            //so the data in the first of the two thens on left of arrow
+            // is not the same as 
+            // data in the second then
+            // as this data in the second then is going to be referring to what got returned
+            // which is the json formatted version
+            console.log(data);//this line and previous are critical to working with APIs
+        })
 }
